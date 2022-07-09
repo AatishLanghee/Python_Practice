bd707d76bf88e83b7009d689c677db18d0d45973
Aatish Langhee <49237031+AatishLanghee@users.noreply.github.com>
2022-07-09 22:51:58 +0530

    Add files via upload

---
+++ LBP094_string_pangrams.py
@@ -0,0 +1,18 @@
+########################################################################################################
+# Question: Pangrams
+
+# Implement a program to check whether the given string pangram or not.
+# A pangram is a string that contains all the letters of the English alphabet.
+# An example of a pangram is "The quick brown fox jumps over the lazy dog"
+
+# input ----> a string from the user
+# con ------> non-empty string
+# output ---> Yes or No
+########################################################################################################
+def first_approach(str_: sr) -> str:
+    pass
+
+
+if __name__ == "__main__":
+    string: str = input("Enter the string :")
+    print(first_approach(string,))
