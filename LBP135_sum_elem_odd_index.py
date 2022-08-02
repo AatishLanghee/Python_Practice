a1dc5e54bc7ca683ab33a58b2131b0a5ce1d1c27
Aatish Langhee <49237031+AatishLanghee@users.noreply.github.com>
2022-08-02 21:19:55 +0530

    Add files via upload

---
+++ LBP135_sum_elem_odd_index.py
@@ -0,0 +1,30 @@
+########################################################################################################
+# Question: sum of elements available at odd index
+
+# Implement a program to find the sum of elements available at odd index locations in an array.
+
+# input ----> size and array elements
+# con ------> no
+# output ---> print sum value
+
+# 5
+# 1 2 3 4 5
+# 0 1 2 3 4 ===> 1,3 ===> 2+4=6
+########################################################################################################
+def first_approach(array_1: list, ) -> int:
+    sum_: int = 0
+    for j in range(len(array_1)):
+        if j % 2 == 1:
+            sum_ += array_1[j]
+    return sum_
+
+
+if __name__ == "__main__":
+    size_t: int = int(input("Enter the size of array: "))
+    first_array: list = []
+    for i in range(size_t):
+        element: int = int(input(f"Enter the {i + 1} element of the first array: "))
+        first_array.append(element)
+
+    print(f"First Array : {first_array}")
+    print(first_approach(first_array, ))
